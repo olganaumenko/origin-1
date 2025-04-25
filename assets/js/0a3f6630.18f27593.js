b88b68441b0125c6c7f4c9b819332feaef4a29e7
Naumenko Olga <o.naumenko@o-naumenko.local>
2025-04-25 17:02:17 +0300

    Deploy website - based on dc3e7c18fa339159ccc745e26a769bb47492c57e

--- assets/js/0a3f6630.18f27593.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4692],{3014:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/origin-1/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
