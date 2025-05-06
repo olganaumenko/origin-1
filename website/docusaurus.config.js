90d53b9c557972618df76e03ff0256d3909faa5f
Naumenko Olga <o.naumenko@o-naumenko.local>
2025-05-06 15:15:53 +0300

    Updated baseURL to /origin-1/

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/origin-1',
+  baseUrl: '/origin-1/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
